9ab0857418f7fe3fec5d4200f65317914d62a23e
sp_3141 <07shourya.pratap@gmail.com>
2026-01-21 22:37:25 +0530

    Added Day 4

--- Day_4/script.js
+++ Day_4/script.js
@@ -1,6 +1,6 @@
 function spreadOperator() {
-  let fruits1= ["apple","banana"];
-  let fruits2 = ["cherry","orange"];
+  let fruits1 = ["apple", "banana"];
+  let fruits2 = ["cherry", "orange"];
   console.log(fruits1 + fruits2);
   console.log(fruits1.concat(fruits2));
   console.log([...fruits1, ...fruits2]);
@@ -8,15 +8,59 @@ function spreadOperator() {
 
 function ternaryOperator() {
   let age = 18;
-  let canVote = age>=18? "Can Vote" : "Can't Vote";
+  let canVote = age >= 18 ? "Can Vote" : "Can't Vote";
   console.log(canVote);
 }
 
-function practice() {
+function leapYear() {
   let num = 101;
-  let isEvenOdd = num%2 == 0? "Even" : "Odd";
+  let isEvenOdd = num % 2 == 0 ? "Even" : "Odd";
   console.log(`${num} is ${isEvenOdd}`);
   let year = 100;
-  let leapYear = (year%4 == 0 && year%100 != 0) || year%400 != 0 ? "leap Year" : "not a Leap Year";
-  console.log(`${year} is ${leapYear}`)
+  let leapYear =
+    (year % 4 == 0 && year % 100 != 0) || year % 400 != 0
+      ? "leap Year"
+      : "not a Leap Year";
+  console.log(`${year} is ${leapYear}`);
 }
+
+// function sample(callback){
+//     console.log("sample");
+//     callback()
+// }
+// function test(){
+//     console.log("test")
+// }
+// sample(test)
+
+function sample() {
+  console.log("sample");
+  return function demo() {
+    console.log("demo");
+    return function test() {
+      console.log("test");
+    };
+  };
+}
+
+// sample()()()
+// var value = sample()
+// let valueTwo = value()
+// valueTwo();
+
+// setTimeout(Callback,delay)
+
+// setInterval(function() {
+//     console.log("Hello world");
+// }, 1000);
+
+function demo() {
+  console.log("demo");
+}
+console.log("sample");
+function sample() {
+  demo();
+}
+sample()(function () {
+  console.log("This is an IIFE.");
+})();
