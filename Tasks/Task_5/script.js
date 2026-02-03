b2029efe66f734f4919d9fa8786ef88196165550
sp_3141 <07shourya.pratap@gmail.com>
2026-02-03 18:24:15 +0530

    Added Task_5

---
+++ Tasks/Task_5/script.js
@@ -0,0 +1,6 @@
+const divs = document.querySelectorAll(".card");
+for (let div of divs) {
+  div.style.border = "2px solid black";
+  div.style.padding = "40px";
+  div.style.borderRadius = "20px";
+}
