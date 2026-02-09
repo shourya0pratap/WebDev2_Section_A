d8639c129293e3e7246388c88308b0baa933599e
sp_3141 <07shourya.pratap@gmail.com>
2026-02-09 22:06:19 +0530

    Added html, css and javascript

--- Assigment_1/script.js
+++ Assigment_1/script.js
@@ -1,4 +1,17 @@
-// target form
-// target buttons
+const form = document.getElementById("eventForm");
+const addBtn = document.getElementById("add-btn");
 
-let sampleEvent = [{}, {}];
+let sampleEvent = [
+  {
+    eventName: "Birthday",
+    eventDate: "2026-02-26",
+    eventDescription: "Happy Birthday!",
+    eventPriority: "Low",
+  },
+  {
+    eventName: "Freshers",
+    eventDate: "2026-02-28",
+    eventDescription: "Freshers Party",
+    eventPriority: "Low",
+  },
+];
