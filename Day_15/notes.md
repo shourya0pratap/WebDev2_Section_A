16a7f6103a10ac2f24d308d11574bb097df30e5e
sp_3141 <07shourya.pratap@gmail.com>
2026-03-02 22:33:20 +0530

    Added Day_15

---
+++ Day_15/notes.md
@@ -0,0 +1,9 @@
+# Promise
+It is an object. It can have one of three states:
+- Pending
+- Resolved
+- Rejected
+
+# Functions
+- .then() - When the promise is resolved it will go to then   
+- .catch() - Otherwise it will go to catch
---
+++ Day_15/script.js
@@ -0,0 +1,28 @@
+// let pro = new Promise((resolve, reject) => {
+//     let a = 10;
+//     if (a > 5) {
+//         resolve()
+//     }
+//     else {
+//         reject()
+//     }
+// })
+
+// pro
+// .then(()=>console.log("Resolved"))
+// .catch((err)=>console.error(err,"Rejected"));
+
+function pro(num) {
+    return new Promise((resolved, rejected)=> {
+        setTimeout(()=> {
+            console.log(num)
+            resolved();
+        }, 1000)
+    });
+}
+
+pro(1)
+.then(()=>pro(2))
+.then(()=>pro(3))
+.then(()=>pro(4))
+.then(()=>pro(5));
