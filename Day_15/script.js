8fb2f5cd09b1d5d9257f69e6dfa4912a2ffbf358
sp_3141 <07shourya.pratap@gmail.com>
2026-03-03 22:38:47 +0530

    Updated script.js

--- Day_15/script.js
+++ Day_15/script.js
@@ -13,16 +13,22 @@
 // .catch((err)=>console.error(err,"Rejected"));
 
 function pro(num) {
-    return new Promise((resolved, rejected)=> {
-        setTimeout(()=> {
-            console.log(num)
-            resolved();
-        }, 1000)
-    });
+  return new Promise((resolved, rejected) => {
+    setTimeout(() => {
+      console.log(num);
+      resolved();
+    }, 1000);
+  });
 }
 
-pro(1)
-.then(()=>pro(2))
-.then(()=>pro(3))
-.then(()=>pro(4))
-.then(()=>pro(5));
+// pro(1)
+// .then(()=>pro(2))
+// .then(()=>pro(3))
+// .then(()=>pro(4))
+// .then(()=>pro(5));
+
+async function run() {
+  for (let i = 1; i < 6; i++) await pro(i);
+}
+
+run();
