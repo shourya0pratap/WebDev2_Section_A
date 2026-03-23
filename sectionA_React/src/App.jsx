d858190d251a48ac478d1901473e8c2ec7663851
sp_3141 <07shourya.pratap@gmail.com>
2026-03-23 19:58:54 +0530

    Added React folder

---
+++ sectionA_React/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello World</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ sectionA_React/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="mainHeader">
+      <div>Logo</div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Course</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
