6019a4c9dca2446a9ee74728d52cb6ddd86e5d41
sp_3141 <07shourya.pratap@gmail.com>
2026-04-01 22:16:01 +0530

    Added styling for counter

--- sectionA_React/src/components/Count.jsx
+++ sectionA_React/src/components/Count.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import styles from "./count.module.css";
 
 const Count = () => {
   const [count, setCount] = useState(0);
   return (
     <>
-      <div>Count: {count}</div>
-      <button onClick={()=>setCount(count+1)}>Click me</button>
+      <div className={styles.d}>Count: {count}</div>
+      <button className={styles.btn} onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </>
   );
 };
